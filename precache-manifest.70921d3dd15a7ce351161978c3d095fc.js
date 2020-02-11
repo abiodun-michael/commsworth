0de322fb62169431f8ec3580dfae54ca56a9bda8
Abiodun Michael <michaelmedewase@gmail.com>
2020-02-11 21:17:17 +0100

    Updates

---
+++ precache-manifest.70921d3dd15a7ce351161978c3d095fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddd6c3b6677ba08a9bb1ff2d6ad6353",
+    "url": "/commsworth/index.html"
+  },
+  {
+    "revision": "5e1ff36c1f8bcb75922e",
+    "url": "/commsworth/static/css/main.6649b116.chunk.css"
+  },
+  {
+    "revision": "68eaea8a5b190db4aa03",
+    "url": "/commsworth/static/js/2.2409725b.chunk.js"
+  },
+  {
+    "revision": "fb226702356a46ed44aa17eb4ca4076e",
+    "url": "/commsworth/static/js/2.2409725b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e1ff36c1f8bcb75922e",
+    "url": "/commsworth/static/js/main.b6c535be.chunk.js"
+  },
+  {
+    "revision": "ceac4176f828b5a689fc",
+    "url": "/commsworth/static/js/runtime-main.ef63a2d1.js"
+  }
+]);
